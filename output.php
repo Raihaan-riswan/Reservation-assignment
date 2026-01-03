1dfb5f3b8ea97693c3a3fa33ac342e21332c5be6
Raihaan Riswan <mohamedraihaanmohamedriswan@gmail.com>
2026-01-03 23:46:01 +0530

    update files

--- output.php
+++ output.php
@@ -1,6 +1,19 @@
 <?php
-    $name = $days = $hotel = $room = $spa = $cycling = $swimming = $gym = $board = ""; 
+    $name = $days = $datein = $dateout = $hotel = $room = $spa = $cycling = $swimming = $gym = $board = ""; 
     $name = $_POST['name'];
+
+    $datein = new DateTime ($_POST['date-in']);
+    $dateout = new DateTime ($_POST['date-out']);
+    $days = $datein ->diff($dateout);
     
+    
+    $hotel = $_POST['hotel'];
+    $room = $_POST['room-type'];
+    if($hotel == 'river_side'){
+        if($room == '')
+    }
+    else{
+        echo("hi2");
+    }
 
 ?>
